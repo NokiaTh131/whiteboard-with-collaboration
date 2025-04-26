a56b6194866f227677bef8a43aec70660f8eabf7
= <=>
2025-04-26 14:21:36 +0700

    feat: implement Suspense for loading state in Home component and integrate LoginPage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Suspense } from "react";
+import LoginPage from "./login/page";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginPage />
+      </Suspense>
+    </div>
+  );
 }
